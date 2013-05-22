3e88b19022425c9c8d539069b743ac3817b62af9
Alex Klyanchin <jetblackmeerkat@gmail.com>
2013-05-22 13:30:38 +0400

    authentication upgrade

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_filter :authenticate_user, :only => [:new, :create]
+  skip_before_filter :authenticate_user!, :only => [:new, :create]
 
   def new
   end
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email params[:email]
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+      sign_in user
       redirect_to root_url, :notice => t('signed_in')
     else
       flash.now.alert = t('invalid_credentials')
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    sign_out
     redirect_to new_session_path
   end
 end
